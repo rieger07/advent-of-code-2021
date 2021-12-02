3decfc71d8ed7e9a2cc82f1ef64ee295e55fc715
Steve Rieger <srieger@idtus.com>
2021-12-02 09:55:36 -0500

    autoformat

--- day2/part2.py
+++ day2/part2.py
@@ -1,9 +1,8 @@
 from submarine import Submarine2, performInstruction
 
 
-
 if __name__ == "__main__":
-    sub = Submarine2(0,0,0)
+    sub = Submarine2(0, 0, 0)
     with open("day2/puzzleinput.txt", "r", encoding="utf-8") as f:
         lines = f.readlines()
     for l in lines:
